398dff746d42015cf7891472229818d4d49ae3ee
oracle727 <daisuke.morita727@gmail.com>
2021-10-06 08:40:15 +0000

    ajax完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book.id)
+    # redirect_to book_path(book.id)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     BookComment.find_by(params[:id]).destroy
-    redirect_to book_path(book.id)
+    # redirect_to book_path(book.id)
   end
 
   private
